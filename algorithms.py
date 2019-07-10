f7a0175e1a021ea273783b15748c7963929ab005
emark1 <devel010@umn.edu>
2019-07-10 03:51:05 -0500

    New

--- algorithms.py
+++ algorithms.py
@@ -147,3 +147,28 @@ def CapitalizeWords(string):
     for i in range(0, len(words)):
         words[i] = words[i][0].upper() + words[i][1:]
     return " ".join(words)
+
+
+#Calculate TIp, assuming 15%
+def CalculateTip(bill):
+    tip = bill * 0.15
+    return tip
+
+#Prime numbers
+def IsPrime(num):
+    # If given number is greater than 1 
+    if num > 1: 
+        
+    # Iterate from 2 to n / 2  
+    for i in range(2, num//2): 
+            
+        # If num is divisible by any number between  
+        # 2 and n / 2, it is not prime  
+        if (num % i) == 0: 
+            print(num, "is not a prime number") 
+            break
+    else: 
+        print(num, "is a prime number") 
+    
+    else: 
+    print(num, "is not a prime number") 
