64767072bafd4ce749a8b01ab126e6ff8cadfc75
emark1 <devel010@umn.edu>
2019-07-17 01:37:25 -0500

    Cleaned up grammar

--- algorithms.py
+++ algorithms.py
@@ -165,10 +165,10 @@ def IsPrime(num):
         # If num is divisible by any number between  
         # 2 and n / 2, it is not prime  
         if (num % i) == 0: 
-            print(num, "is not a prime number") 
+            print(num, " is not a prime number") 
             break
     else: 
-        print(num, "is a prime number") 
+        print(num, " is a prime number") 
     
     else: 
-    print(num, "is not a prime number.") 
+    print(num, " is not a prime number.") 
