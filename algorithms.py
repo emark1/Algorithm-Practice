4f892a940f036e9fb746bf8ed0f426742604b1ee
emark1 <devel010@umn.edu>
2019-07-28 23:40:16 -0500

    Biggest number

--- algorithms.py
+++ algorithms.py
@@ -181,3 +181,20 @@ def MissingElement():
       print("Missing " + str(element) + "!")
 
 MissingElement()
+
+word_list = ["Me","Bike","Minnesota","Apple"]
+
+def determine_longest(list):
+    longestword = max(list, key=len)
+    return longestword
+
+print(determine_longest(word_list))
+
+#Find the biggest number
+numbers = [15,10,100,1]
+
+def biggest_number(list):
+    bignumber = max(list)
+    return bignumber
+
+print(biggest_number(numbers))
