28fb91e551b58f83de1c5857e9e8d37003d42478
emark1 <devel010@umn.edu>
2019-08-04 23:55:26 -0500

    Another bubblesort

--- algorithms.py
+++ algorithms.py
@@ -240,3 +240,16 @@ def BubbleSort(numlist):
                 tempnum = list[i]               
                 list[i] = list[i+1]
                 list[i+1] = tempnum
+
+def shortBubbleSort(alist):
+    exchanges = True
+    passnum = len(alist)-1
+    while passnum > 0 and exchanges:
+       exchanges = False
+       for i in range(passnum):
+           if alist[i]>alist[i+1]:
+               exchanges = True
+               temp = alist[i]
+               alist[i] = alist[i+1]
+               alist[i+1] = temp
+       passnum = passnum-1
